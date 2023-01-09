5dc1d79e4e8ec779e097067ee8e9dd419f6898e0
jj <jj@9100f>
2023-01-09 01:47:24 +0100

    hash table for duplicated 4k blocs finished

--- compressor.cpp
+++ compressor.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cstring>
 
 Compressor::Compressor(vector<string> &input_files)
 {
@@ -24,6 +25,7 @@ Compressor::Compressor(vector<string> &input_files)
 		id_file++;
 	}
 	last_bloc_id_=id_bloc;
+	SameBlocs();
 	//ParseBlocs();
 }
 
@@ -62,15 +64,30 @@ void *Compressor::AddressByGlobalBlocId(u32 id_bloc)
 	return pt_bloc;
 }
 
+bool Compressor::CompareBlocs(u32 b1,u32 b2)
+{
+	void* a1=AddressByGlobalBlocId(b1);
+	void* a2=AddressByGlobalBlocId(b2);
+
+	int ret=memcmp(a1,a2,4096);
+	return ret==0;
+}
+
 // 4k same bloc
 void Compressor::SameBlocs()
 {
 	// we use a table with as many entries as we have 4k blocs
-	HashTable4k table(last_bloc_id_);
+	// TBD : add an argument to command line to define table size
+	HashTable4k table(last_bloc_id_+1,*this);
 
 	for (u32 i=0;i<last_bloc_id_;i++)
 	{
-
+		u32 ii=table.ProcessBloc(i);
+		if (ii!=0xffffffff)
+		{
+			// duplicated bloc, we need to keep track of it, as we won't work on it anymore for further comp.
+			bloc_is_same_than_[i]=ii;
+		}
 	}
 }
 
--- compressor.h
+++ compressor.h
@@ -17,6 +17,7 @@ public:
 	Compressor(vector<string> &input_files);
 	File* FileByGlobalBlocId(u32 id_bloc);
 	void* AddressByGlobalBlocId(u32 id_bloc);
+	bool CompareBlocs(u32 b1,u32 b2);
 	void ParseBlocs();
 
 private:
@@ -29,6 +30,7 @@ private:
 	map<u32,File*> tree_4k_;
 	u32 last_bloc_id_;
 
+	map<u32,u32> bloc_is_same_than_;
 };
 
 #endif
